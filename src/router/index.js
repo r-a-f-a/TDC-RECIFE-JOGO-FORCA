264045053bce75e26ca87eaaeb9234cfc7cd3098
Renato Novaes <renatoalmeida49@gmail.com>
2021-06-01 18:58:34 -0300

    Alteracao no router

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,6 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Start',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/start')
-  },
-  {
-    path: '/game',
     name: 'Game',
     component: () => import('../components/game')
   }
